4f80d1ee05662c5965294deedc4ce91ff8994fa8
rk00717 <58892434+rk00717@users.noreply.github.com>
2025-07-27 00:18:23 +0530

    Implemented Basic Password Generator

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from engine import PassGenerator, CryptoTable
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    password = ''
+    if request.method == 'POST':
+        length = int(request.form.get('length', 12))
+        platform_name = str(request.form.get('platform_name', ""))
+        username = str(request.form.get('username', ""))
+
+        table_size = int(request.form.get('table_size', 0))
+        length = int(request.form.get('length', 0))
+        salt_1 = int(request.form.get('salt_1', 0))
+        salt_2 = int(request.form.get('salt_2', 0))
+
+        use_upper = 'upper' in request.form
+        use_lower = 'lower' in request.form
+        use_digits = 'digits' in request.form
+        use_symbols = 'symbols' in request.form
+
+        cryptoTable = CryptoTable(table_size)
+        generator = PassGenerator(length, salt_1, salt_2)
+
+        password = str("")
+        if not(table_size == 0 or length == 0 or salt_1 == 0 or salt_2 == 0) :
+            password = generator.generate_password(cryptoTable.table, platform_name, username)
+
+        return render_template('index.html', password=password)
+
     return render_template('index.html')
 
 if __name__ == '__main__':
---
+++ engine/__init__.py
@@ -0,0 +1,63 @@
+import os
+import json
+
+class UserData:
+    def __init__(self, platform, username):
+        self.username = username
+        self.platform = platform
+
+    def set_password(self, password):
+        self.password = password
+
+class CryptoTable:
+    symbols = (
+        list("abcdefghijklmnopqrstuvwxyz") +
+        list("ABCDEFGHIJKLMNOPQRSTUVWXYZ") +
+        list("0123456789") +
+        list("@#$%&*!")
+    )
+
+    def __init__(self, table_key):
+        self.key = table_key
+
+        self.columns = 8
+        self.table = self.shuffle_list()
+
+    def shuffle_list(self):
+        shuffled_list = [(symbol, (ord(symbol) * self.key) % len(self.symbols)) for symbol in self.symbols]
+        shuffled_list.sort(key=lambda x:x[1])
+        shuffled_symbols = [symbol[0] for symbol in shuffled_list]
+        return shuffled_symbols
+        
+    def display(self):
+        col = self.columns
+        display_table = [self.table[i:i + col] for i in range(0, len(self.symbols), col)]
+        [print("| {0} |".format(" | ".join(row))) for row in display_table]
+
+    def calculate_sum(self, content):
+        return sum((ord(char) - ord("a") + 1) if char.islower() else (ord(char) - ord("A") + 1) for char in content)
+    
+class PassGenerator:
+    def __init__(self, password_size, key_1, key_2):
+        self.password_size = password_size
+        self.key_1 = key_1
+        self.key_2 = key_2
+
+    def generate_password(self, table:list, platform:str, username:str, skip = 0):
+        password = []
+        platform_rotated = platform
+        username_rotated = username
+
+        for _ in range(self.password_size):
+            platform_sum = ord(platform_rotated[0]) + skip
+            username_sum = ord(username_rotated[0]) + skip
+            
+            row = username_sum % self.key_2
+            col = platform_sum % self.key_1
+            index = (row * self.key_1 + col) % len(table)
+
+            password.append(table[index])
+            platform_rotated = platform_rotated[1:] + platform_rotated[0]
+            username_rotated = username_rotated[1:] + username_rotated[0]
+
+        return ''.join(password)
